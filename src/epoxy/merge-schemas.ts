1630e8f4e7662f624dab543b5525b9b1117235d4
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-02-12 16:55:31 -0500

    Enhance mergeSchemas config

--- src/epoxy/merge-schemas.ts
+++ src/epoxy/merge-schemas.ts
@@ -1,32 +1,43 @@
 import { GraphQLSchema, DocumentNode } from "graphql";
-import { IResolvers, SchemaDirectiveVisitor, makeExecutableSchema, IResolverValidationOptions } from "graphql-tools";
+import { IResolvers, SchemaDirectiveVisitor, makeExecutableSchema, IResolverValidationOptions, ILogger } from "graphql-tools";
 import { mergeTypeDefs } from "./typedefs-mergers/merge-typedefs";
 import { asArray } from "../utils/helpers";
 import { mergeResolvers } from "./resolvers-mergers/merge-resolvers";
-import { extractResolversFromSchema } from "../utils";
+import { extractResolversFromSchema, IResolversComposerMapping, composeResolvers } from "../utils";
 
 export interface MergeSchemasConfig {
     schemas: GraphQLSchema[];
     typeDefs?: (DocumentNode | string)[] | DocumentNode | string;
     resolvers?: IResolvers | IResolvers[];
+    resolversComposition?: IResolversComposerMapping;
     schemaDirectives ?: { [directiveName: string] : typeof SchemaDirectiveVisitor };
     resolverValidationOptions ?: IResolverValidationOptions;
+    logger?: ILogger;
 }
 
-export function mergeSchemas(config: MergeSchemasConfig) {
-    const typeDefs = mergeTypeDefs([
-        ...config.schemas,
-        ...config.typeDefs ? asArray(config.typeDefs) : []
-    ]);
-    const resolvers = mergeResolvers([
-        ...config.schemas.map(schema => extractResolversFromSchema(schema)),
-        ...config.resolvers ? asArray<IResolvers>(config.resolvers) : []
-    ]);
-    
+export function mergeSchemas({
+    schemas,
+    typeDefs,
+    resolvers,
+    resolversComposition,
+    schemaDirectives,
+    resolverValidationOptions,
+    logger
+}: MergeSchemasConfig) {
     return makeExecutableSchema({
-        typeDefs,
-        resolvers,
-        schemaDirectives: config.schemaDirectives,
-        resolverValidationOptions: config.resolverValidationOptions
+        typeDefs: mergeTypeDefs([
+            ...schemas,
+            ...typeDefs ? asArray(typeDefs) : []
+        ]),
+        resolvers: composeResolvers(
+                mergeResolvers([
+                ...schemas.map(schema => extractResolversFromSchema(schema)),
+                ...resolvers ? asArray<IResolvers>(resolvers) : []
+            ]),
+            resolversComposition || {}
+        ),
+        schemaDirectives,
+        resolverValidationOptions,
+        logger
     })
 }
