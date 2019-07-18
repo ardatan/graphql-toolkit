db302e0449b9597f93b1adb7ed066a6155715298
Dotan Simha <dotansimha@gmail.com>
2019-07-18 07:56:41 +0300

    added junit reports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+        addFileAttribute: 'true',
+      },
+    ],
+  ],
   globals: {
     'ts-jest': {
       diagnostics: false,
