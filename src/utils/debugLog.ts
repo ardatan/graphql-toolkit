d16d9413b1c57a1786b5d2d95a9ad74d9dec2b2b
Burak Shen <tobuba2@gmail.com>
2019-08-07 04:20:38 +0300

    process.env.DEBUG (#199)
    I don't want to receive toolkit logs, but when I set the value DEBUG to undefined it affects the whole project

--- src/utils/debugLog.ts
+++ src/utils/debugLog.ts
@@ -1,5 +1,5 @@
 export function debugLog(...args: any[]): void {
-  if (process && process.env && process.env.DEBUG) {
+  if (process && process.env && process.env.DEBUG && !process.env.GQL_TOOLKIT_NODEBUG) {
     console.log(...args);
   }
 }
