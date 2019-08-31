fcefd36fbc30997da3034f92be5cac259685676e
Dotan Simha <dotansimha@gmail.com>
2019-08-31 11:26:29 +0300

    better documents validation

--- src/utils/validate-documents.ts
+++ src/utils/validate-documents.ts
@@ -1,22 +1,43 @@
 import * as AggregateError from 'aggregate-error';
-import { validate, GraphQLSchema, GraphQLError, specifiedRules } from 'graphql';
+import { Kind, concatAST, validate, GraphQLSchema, GraphQLError, specifiedRules, FragmentDefinitionNode } from 'graphql';
 import { DocumentFile } from '../loaders/load-typedefs';
 
-const rulesToIgnore = ['KnownFragmentNames', 'NoUnusedFragments', 'NoUnusedVariables', 'KnownDirectives'];
-const effectiveRules = specifiedRules.filter((f: Function) => !rulesToIgnore.includes(f.name));
+const DEFAULT_IGNORED_RULES = ['NoUnusedFragments', 'NoUnusedVariables', 'KnownDirectives'];
+const DEFAULT_EFFECTIVE_RULES = specifiedRules.filter((f: Function) => !DEFAULT_IGNORED_RULES.includes(f.name));
 
 export interface LoadDocumentError {
   readonly filePath: string;
   readonly errors: ReadonlyArray<GraphQLError>;
 }
 
-export const validateGraphQlDocuments = (schema: GraphQLSchema, documentFiles: DocumentFile[]): ReadonlyArray<LoadDocumentError> =>
-  documentFiles
-    .map(result => ({
-      filePath: result.filePath,
-      errors: validate(schema, result.content, effectiveRules),
-    }))
+export const validateGraphQlDocuments = (schema: GraphQLSchema, documentFiles: DocumentFile[], effectiveRules: typeof specifiedRules = DEFAULT_EFFECTIVE_RULES): ReadonlyArray<LoadDocumentError> => {
+  const allAst = concatAST(documentFiles.map(m => m.content));
+  const allFragments = allAst.definitions.filter(d => d.kind === Kind.FRAGMENT_DEFINITION) as FragmentDefinitionNode[];
+
+  return documentFiles
+    .map(file => {
+      const documentToValidate = {
+        kind: Kind.DOCUMENT,
+        definitions: [...allFragments, ...file.content.definitions].filter((d, index, arr) => {
+          if (d.kind === Kind.FRAGMENT_DEFINITION) {
+            const foundIndex = arr.findIndex(i => i.kind === Kind.FRAGMENT_DEFINITION && i.name.value === d.name.value);
+
+            if (foundIndex !== index) {
+              return false;
+            }
+          }
+
+          return true;
+        }),
+      };
+
+      return {
+        filePath: file.filePath,
+        errors: validate(schema, documentToValidate, effectiveRules),
+      };
+    })
     .filter(r => r.errors.length > 0);
+};
 
 export function checkValidationErrors(loadDocumentErrors: ReadonlyArray<LoadDocumentError>): void | never {
   if (loadDocumentErrors.length > 0) {
@@ -24,19 +45,16 @@ export function checkValidationErrors(loadDocumentErrors: ReadonlyArray<LoadDocu
 
     for (const loadDocumentError of loadDocumentErrors) {
       for (const graphQLError of loadDocumentError.errors) {
-        const error = new Error()
-        error.name = 'GraphQLDocumentError'
-        error.message = `${error.name}: ${graphQLError.message}`
-        error.stack = error.message
-
-        graphQLError.locations.forEach(location =>
-          error.stack += `\n    at ${loadDocumentError.filePath}:${location.line}:${location.column}`
-        );
+        const error = new Error();
+        error.name = 'GraphQLDocumentError';
+        error.message = `${error.name}: ${graphQLError.message}`;
+        error.stack = error.message;
+        graphQLError.locations.forEach(location => (error.stack += `\n    at ${loadDocumentError.filePath}:${location.line}:${location.column}`));
 
         errors.push(error);
       }
     }
-    
+
     throw new AggregateError(errors);
   }
 }
