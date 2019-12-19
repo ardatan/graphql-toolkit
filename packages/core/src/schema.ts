65e2e63de0fb79309f2934abf5796d1470fabdde
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-12-19 02:15:55 -0500

    Able to pass BuildSchemaOptions in schema loaders options

--- packages/core/src/schema.ts
+++ packages/core/src/schema.ts
@@ -1,11 +1,13 @@
 import { loadTypedefsUsingLoaders, LoadTypedefsOptions, UnnormalizedTypeDefPointer } from './load-typedefs';
-import { buildASTSchema, GraphQLSchema } from 'graphql';
+import { buildASTSchema, GraphQLSchema, BuildSchemaOptions } from 'graphql';
 import { OPERATION_KINDS } from './documents';
 import { mergeTypeDefs } from '@graphql-toolkit/schema-merging';
 import { Loader } from '@graphql-toolkit/common';
 
-export async function loadSchemaUsingLoaders(loaders: Loader[], schemaPointers: UnnormalizedTypeDefPointer | UnnormalizedTypeDefPointer[], options?: LoadTypedefsOptions, cwd = process.cwd()): Promise<GraphQLSchema> {
+export type LoadSchemaOptions = BuildSchemaOptions & LoadTypedefsOptions;
+
+export async function loadSchemaUsingLoaders(loaders: Loader[], schemaPointers: UnnormalizedTypeDefPointer | UnnormalizedTypeDefPointer[], options?: LoadSchemaOptions, cwd = process.cwd()): Promise<GraphQLSchema> {
   const types = await loadTypedefsUsingLoaders(loaders, schemaPointers, options, OPERATION_KINDS, cwd);
 
-  return buildASTSchema(mergeTypeDefs(types.map(m => m.document)));
+  return buildASTSchema(mergeTypeDefs(types.map(m => m.document)), options);
 }
